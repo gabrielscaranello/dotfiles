273d4d5a3aebee0fe5a043298445dac47371d0f2
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-04-10 23:25:16 -0300

    [nvim] prefer use self configured cutlass plugin

---
+++ config/nvim/lua/plugins/cutlass.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+return {
+  "gbprod/cutlass.nvim",
+  opts = {
+    cut_key = "x",
+    override_del = nil,
+    registers = { select = "_", delete = "_", change = "_" },
+  },
+}
