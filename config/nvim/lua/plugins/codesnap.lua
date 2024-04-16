b897789453792d90ae55dd6f2c4190563de62e48
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-04-16 02:52:06 -0300

    [nvim] improve code experience

---
+++ config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+return {
+  "mistricky/codesnap.nvim",
+  opts = {
+    mac_window_bar = false,
+    watermark = "",
+    bg_color = "#181825",
+    save_path = "~/Code/Snapshots",
+    code_font_family = "JetBrainsMono Nerd Font",
+  },
+}
