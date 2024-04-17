aa387fe4e177e297097b3c90c65c3768c5dcb86e
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-04-17 09:50:37 -0300

    [nvim] add git file types to treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,11 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
       "make",
       "proto",
       "scss",
