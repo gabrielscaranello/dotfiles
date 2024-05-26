ae30f771db983a64c3e8adf8cb610cfaaaa9ca3a
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-05-26 17:23:52 -0300

    fix(discord): launch discord error on fedora

--- actions/discord.sh
+++ actions/discord.sh
@@ -9,7 +9,7 @@ echo "Creating config directory..."
 mkdir -p "$CONFIG_DIR"
 
 echo "Starting discord..."
-/usr/bin/discord --start-minimized >/dev/null 2>&1 &
+if [ -f "/usr/bin/discord" ]; then /usr/bin/discord --start-minimized >/dev/null 2>&1 & elif [ -f "/usr/bin/Discord" ]; then /usr/bin/Discord --start-minimized >/dev/null 2>&1 & fi
 sleep 2
 
 echo "Installing catppuccin-mocha-blue theme..."
@@ -19,7 +19,7 @@ echo "Installing beautifuldiscord..."
 PIP_BREAK_SYSTEM_PACKAGES=1 python3 -m pip install -U https://github.com/leovoel/BeautifulDiscord/archive/master.zip
 
 echo "Setting up beautifuldiscord..."
-python3 -m beautifuldiscord --css "$CONFIG_DIR/catppuccin-mocha-blue.theme.css"
+python3 -m beautifuldiscord --css "$CONFIG_DIR/catppuccin-mocha-blue.theme.css" >/dev/null
 
 kill $(pidof -s Discord)
 
