f5d52f8b1ed05e5e5cfbf788c0d7a4f2f0ab3dbc
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-06-18 10:11:16 -0300

    feat(nvim): use custom config of cmp

---
+++ config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,17 @@
+---@type LazySpec
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require "cmp"
+
+    opts.sources = cmp.config.sources {
+      { name = "codeium", priority = 1250 },
+      { name = "nvim_lsp", priority = 1000 },
+      { name = "luasnip", priority = 750 },
+      { name = "buffer", priority = 500 },
+      { name = "path", priority = 250 },
+    }
+
+    return opts
+  end,
+}
