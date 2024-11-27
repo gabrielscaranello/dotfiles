58cbae7968e855b2b50624e4ac8fcdfb1a2b83ad
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-11-27 10:23:41 -0300

    feat(nvim): add autopairs plugin

---
+++ config/nvim-self/lua/plugins/autopairs.lua
@@ -0,0 +1,25 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  },
+  after = "nvim-cmp",
+  opts = {
+    check_ts = true,
+    ts_config = {
+      lua = { "string", "source" },
+      javascript = { "string", "template_string" },
+      java = false,
+    },
+  },
+
+  config = function(_, opts)
+    local autopairs = require "nvim-autopairs"
+    local cmp = require "cmp"
+    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+
+    autopairs.setup(opts)
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
+}
