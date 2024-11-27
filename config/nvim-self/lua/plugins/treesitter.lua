764b030220a75dc314ae557ba9ba6a50c27a8ab8
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-11-27 10:50:46 -0300

    feat(nvim): update treesitter config to use autotag and add more langs

--- config/nvim-self/lua/plugins/treesitter.lua
+++ config/nvim-self/lua/plugins/treesitter.lua
@@ -1,29 +1,52 @@
 local ensure_installed = {
+  "css",
+  "html",
+  "javascript",
+  "json",
   "lua",
+  "markdown_inline",
+  "markdown",
+  "query",
+  "regex",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
 }
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  version = false,
-  lazy = vim.fn.argc(-1) == 0,
-  event = { "BufReadPost", "BufNewFile" },
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  opts_extend = { "ensure_installed" },
-  opts = {
-    ensure_installed = ensure_installed,
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    opts = {},
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    version = false,
+    lazy = vim.fn.argc(-1) == 0,
+    event = { "BufReadPre", "BufNewFile" },
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+    },
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = ensure_installed,
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
       },
     },
-  },
 
-  config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
+    config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
+  },
 }
