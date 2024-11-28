39391df87e67392ca65c0291dde4cee805691c0a
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-11-28 10:10:57 -0300

    feat(nvim): add trouble plugin

---
+++ config/nvim-self/lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/trouble.nvim",
+  cmd = { "Trouble", "TroubleClose", "TroubleToggle", "TroubleRefresh" },
+  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  event = { "BufReadPre", "BufNewFile" },
+  keys = {
+    { "<leader>xx", "<cmd>Trouble<cr>", desc = "Open/Close Trouble List" },
+    { "<leader>xw", "<cmd>Trouble diagnostics toggle<cr>", desc = "Workspace Diagnostics" },
+    { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Document Diagnostics" },
+    { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List" },
+    { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List" },
+    { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Open Todos in Trouble" },
+  },
+  opts = {},
+}
