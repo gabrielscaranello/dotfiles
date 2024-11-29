d970da607ae1fe7e39c41cbe2c85062d9ec8d312
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-11-29 17:27:48 -0300

    feat(nvim): add cutlass plugin

---
+++ config/nvim-self/lua/plugins/cutlass.lua
@@ -0,0 +1,9 @@
+return {
+  "gbprod/cutlass.nvim",
+  event = "VeryLazy",
+  opts = {
+    cut_key = "x",
+    override_del = nil,
+    registers = { select = "_", delete = "_", change = "_" },
+  },
+}
