a0ad27a0cfaf12f7bc9e65c0f899845e8616b58e
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-11-29 14:51:03 -0300

    feat(nvim): add wakatime plugin

--- config/nvim-self/lua/plugins/misc.lua
+++ config/nvim-self/lua/plugins/misc.lua
@@ -3,4 +3,5 @@ return {
   { "nvim-tree/nvim-web-devicons", event = "VeryLazy" },
   { "MunifTanjim/nui.nvim", event = "VeryLazy" },
   { "echasnovski/mini.icons", event = "VeryLazy", version = "*" },
+  { "wakatime/vim-wakatime", event = { "BufReadPre" } },
 }
