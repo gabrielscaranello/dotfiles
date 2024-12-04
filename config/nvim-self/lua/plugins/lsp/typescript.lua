d12da659dc83c5d4d123b4122e4576f76282108d
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-12-03 23:21:45 -0300

    feat(nvim): update typescript support

---
+++ config/nvim-self/lua/plugins/lsp/typescript.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "vuki656/package-info.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    event = "BufRead package.json",
+    opts = {
+      autostart = true,
+    },
+    config = function(_, opts) require("package-info").setup(opts) end,
+  },
+
+  {
+    "yioneko/nvim-vtsls",
+    init = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("nvim_vtsls", {}),
+        callback = function(args)
+          if assert(vim.lsp.get_client_by_id(args.data.client_id)).name == "vtsls" then
+            require("vtsls")._on_attach(args.data.client_id, args.buf)
+            vim.api.nvim_del_augroup_by_name "nvim_vtsls"
+          end
+        end,
+      })
+    end,
+    config = function(_, opts) require("vtsls").config(opts) end,
+  },
+}
