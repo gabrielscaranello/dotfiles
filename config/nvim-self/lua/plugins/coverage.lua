bc3c308a7b1db292b5aa548ea4aea705b9a078fd
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-12-04 21:28:53 -0300

    feat(nvim) add coverage package

---
+++ config/nvim-self/lua/plugins/coverage.lua
@@ -0,0 +1,36 @@
+return {
+  "andythigpen/nvim-coverage",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    auto_reload = true,
+    commands = true,
+  },
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {
+      "<leader>lc",
+      function()
+        local coverage = require "coverage"
+
+        if vim.g.coverage_showing then
+          coverage.hide()
+        else
+          coverage.load(true)
+          coverage.show()
+        end
+
+        vim.g.coverage_showing = not vim.g.coverage_showing
+      end,
+      desc = "Coverage",
+    },
+    {
+      "<leader>lC",
+      function()
+        local coverage = require "coverage"
+        if not vim.g.coverage_showing then coverage.load(false) end
+        coverage.summary()
+      end,
+      desc = "Coverage Summary",
+    },
+  },
+}
