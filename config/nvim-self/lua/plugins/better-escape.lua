bcf46d02bf4977d5367bbd99f2121615b20adde1
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-12-11 18:49:17 -0300

    fix(nvim): better-escape issue in visual mode

--- config/nvim-self/lua/plugins/better-escape.lua
+++ config/nvim-self/lua/plugins/better-escape.lua
@@ -3,6 +3,10 @@ return {
   event = "VeryLazy",
   opts = {
     timeout = vim.o.timeoutlen,
+    mappings = {
+      v = { j = { k = false } },
+      s = { j = { k = false } },
+    },
     default_mappings = true,
   },
 }
