e5efd1e1615bc6d26e726669276b7b66b61eda3d
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-12-12 23:06:30 -0300

    fix(nvim): scroll errors with mini-animate

--- config/nvim-self/lua/plugins/mini-animate.lua
+++ config/nvim-self/lua/plugins/mini-animate.lua
@@ -5,34 +5,12 @@ return {
   opts = function()
     local animate = require "mini.animate"
 
-    local mouse_scrolled = false
-    for _, scroll in ipairs { "Up", "Down" } do
-      local key = "<ScrollWheel" .. scroll .. ">"
-      vim.keymap.set({ "", "i" }, key, function()
-        mouse_scrolled = true
-        return key
-      end, { expr = true })
-    end
-
     return {
       open = { enable = false },
       close = { enable = false },
       resize = { enable = false },
-      scroll = {
-        timing = animate.gen_timing.linear { duration = 150, unit = "total" },
-        subscroll = animate.gen_subscroll.equal {
-          predicate = function(total_scroll)
-            if not mouse_scrolled then
-              mouse_scrolled = false
-              return false
-            end
-            return total_scroll > 1
-          end,
-        },
-      },
-      cursor = {
-        timing = animate.gen_timing.linear { duration = 80, unit = "total" },
-      },
+      scroll = { enable = false },
+      cursor = { timing = animate.gen_timing.linear { duration = 80, unit = "total" } },
     }
   end,
   config = function(_, opts) require("mini.animate").setup(opts) end,
