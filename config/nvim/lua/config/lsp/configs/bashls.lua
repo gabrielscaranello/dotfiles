a2e7ffebc4291b03cf1e15ebb56f586b88ff3b34
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-01-02 01:49:30 -0300

    fix(nvim): bashls showing document symbol in some times

--- config/nvim/lua/config/lsp/configs/bashls.lua
+++ config/nvim/lua/config/lsp/configs/bashls.lua
@@ -2,7 +2,5 @@ return {
   on_attach = function(client)
     local filename = vim.fn.expand "%:t"
     if filename:match "^%.env" then return end
-
-    if client.server_capabilities.documentSymbolProvider then vim.lsp.buf.document_symbol() end
   end,
 }
