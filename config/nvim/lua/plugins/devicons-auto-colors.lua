a114b8860eada7e5891aa0a3160ebdd79a1297cd
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-01-26 14:35:44 -0300

    chore(nvim): update lazy-lock

--- config/nvim/lua/plugins/devicons-auto-colors.lua
+++ config/nvim/lua/plugins/devicons-auto-colors.lua
@@ -2,11 +2,10 @@ return {
   "rachartier/tiny-devicons-auto-colors.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
-    "catppuccin/nvim",
   },
   event = "VeryLazy",
   config = function()
-    local theme_colors = require("catppuccin.palettes").get_palette "mocha"
+    local theme_colors = require("catppuccin.palettes").get_palette()
 
     require("tiny-devicons-auto-colors").setup {
       colors = theme_colors,
