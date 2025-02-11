9b75518601a6f1b011a18a23ea7aa7049b49c03e
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-02-11 10:15:37 -0300

    fix(nvim): upper change and upper delete has not send value to black hole

--- config/nvim/lua/plugins/cutlass.lua
+++ config/nvim/lua/plugins/cutlass.lua
@@ -7,4 +7,15 @@ return {
     exclude = {},
     registers = { select = "_", delete = "_", change = "_" },
   },
+  init = function()
+    local keymap_opts = { noremap = true, silent = true }
+    local keymap = require "utils.keymap"
+
+    keymap.map {
+      { "n", "C", '"_C', keymap_opts },
+      { "n", "D", '"_D', keymap_opts },
+      { "x", "C", '"_C', keymap_opts },
+      { "x", "D", '"_D', keymap_opts },
+    }
+  end,
 }
