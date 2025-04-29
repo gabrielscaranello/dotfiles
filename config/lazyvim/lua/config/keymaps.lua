9bb7ea63e6635b6995587349a1bf42877432ce21
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-04-28 21:59:12 -0300

    chore(nvim): add LazyExtra plugins

--- config/lazyvim/lua/config/keymaps.lua
+++ config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>o", function()
+  Snacks.explorer.open()
+end, { silent = true, desc = "Open Explorer" })
