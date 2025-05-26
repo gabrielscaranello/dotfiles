b29148b31b367bea90df99882dcdfbe01366eb08
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-05-26 15:31:52 -0300

    chore(nvim): remove keepcursor

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -6,7 +6,6 @@ return {
   { "junegunn/vim-easy-align", event = "VeryLazy" },
   { "nvim-lua/plenary.nvim", event = "VeryLazy" },
   { "nvim-tree/nvim-web-devicons", event = "VeryLazy" },
-  { "rlychrisg/keepcursor.nvim", event = "VeryLazy", opts = { enabled_on_start_v = "middle" } },
   { "stevearc/dressing.nvim", event = "VeryLazy", dependencies = { "MunifTanjim/nui.nvim" }, opts = {} },
   { "stevearc/stickybuf.nvim", event = "VeryLazy", opts = {}, cmd = { "PinBuffer", "PinBuftype", "PinFiletype" } },
   { "wakatime/vim-wakatime", event = { "BufReadPre" } },
