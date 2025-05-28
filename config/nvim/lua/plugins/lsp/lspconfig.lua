4f91cf2870231f64cd2e848c28ff1fe6b230e5de
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-05-28 10:47:22 -0300

    fix(nvim): remove lsp setup redundancy

--- config/nvim/lua/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -38,6 +38,7 @@ return {
   },
 
   config = function(_, opts)
+    local automatic_enable_exclude = { "vue_ls" }
     local lspconfig = require "lspconfig"
     local mason_lspconfig = require "mason-lspconfig"
     local cmp_nvim_lsp = require "cmp_nvim_lsp"
@@ -52,6 +53,7 @@ return {
     local function setup_lsp(server_name)
       local config = lsp_setup.mkconfig(server_name, { capabilities = capabilities })
       lspconfig[server_name].setup(config)
+      table.insert(automatic_enable_exclude, server_name)
     end
 
     for server_name in pairs(lsp_setup.configs) do
@@ -60,7 +62,7 @@ return {
 
     ---@diagnostic disable-next-line: missing-fields
     mason_lspconfig.setup {
-      automatic_enable = { exclude = { "vue_ls" } },
+      automatic_enable = { exclude = automatic_enable_exclude },
     }
   end,
 }
