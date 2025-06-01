acc92d9b94b57658fd7e11a79e1ba32f1e2b66ec
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-06-01 18:39:44 -0300

    refactor(nvim): replace Comment.nvim as mini-comment

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,21 +1,11 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+  "echasnovski/mini.comment",
+  event = "VeryLazy",
   keys = {
-    { "/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", desc = "Toggle comment for current line" },
-    {
-      "/",
-      ":<C-u>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-      desc = "Toggle comment for selection",
-      mode = "v",
-    },
+    { "/", "gcc", remap = true, desc = "Toggle comment for current line" },
+    { "/", "gc", remap = true, desc = "Toggle comment for selection", mode = "v" },
+    { "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", desc = "Add Comment Below" },
+    { "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", desc = "Add Comment Above" },
   },
-  opts = function()
-    local ts_context_commentstring = require "ts_context_commentstring.integrations.comment_nvim"
-
-    return {
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    }
-  end,
+  opts = {},
 }
