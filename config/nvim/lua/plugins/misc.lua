c8c73d224b63571b1017da79d568f4eba0e7eab1
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-06-02 16:44:16 -0300

    chore(nvim): remove redundant plugins

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -1,9 +1,5 @@
 return {
-  { "MunifTanjim/nui.nvim", event = "VeryLazy" },
   { "b0o/SchemaStore.nvim", event = "VeryLazy" },
-  { "echasnovski/mini.icons", event = "VeryLazy", version = "*" },
-  { "folke/edgy.nvim", event = "VeryLazy", opts = {} },
   { "junegunn/vim-easy-align", event = "VeryLazy" },
-  { "nvim-lua/plenary.nvim", event = "VeryLazy" },
   { "wakatime/vim-wakatime", event = { "BufReadPre" } },
 }
