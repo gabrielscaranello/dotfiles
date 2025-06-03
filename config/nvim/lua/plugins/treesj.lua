a34e6594bba383938949e7b3d9bcb96e339aa80a
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-06-02 21:56:05 -0300

    refactor(nvim): replace mini-splitjoin as treesj

---
+++ config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,10 @@
+return {
+  "Wansmer/treesj",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  keys = {
+    { "<leader>lo", function() require("treesj").toggle() end, desc = "Toggle Treesitter Join" },
+  },
+  opts = {
+    use_default_keymaps = false,
+  },
+}
