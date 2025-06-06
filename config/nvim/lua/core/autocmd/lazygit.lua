1aa9b00e5385e331da921b285aaa13ffee730124
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-06-06 12:15:29 -0300

    fix(nvim): error after close lazygit when buffer is modified

--- config/nvim/lua/core/autocmd/lazygit.lua
+++ config/nvim/lua/core/autocmd/lazygit.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_create_autocmd("TermClose", {
   pattern = "*lazygit*",
   callback = function()
+    if vim.bo.modified then return end
     vim.cmd.bufdo "e"
     require("lazy.core.loader").reload "gitsigns.nvim"
   end,
