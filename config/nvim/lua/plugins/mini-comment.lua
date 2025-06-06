7cdb59baf710f5718286f6c916489da67426b45f
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-06-05 21:55:07 -0300

    feat(nvim): add support comment tscontext

--- config/nvim/lua/plugins/mini-comment.lua
+++ config/nvim/lua/plugins/mini-comment.lua
@@ -1,11 +1,21 @@
 return {
   "echasnovski/mini.comment",
   event = "VeryLazy",
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    opts = { enable_autocmd = true },
+  },
   keys = {
     { "/", "gcc", remap = true, desc = "Toggle comment for current line" },
     { "/", "gc", remap = true, desc = "Toggle comment for selection", mode = "v" },
     { "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", desc = "Add Comment Below" },
     { "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", desc = "Add Comment Above" },
   },
-  opts = {},
+  opts = {
+    options = {
+      custom_commentstring = function()
+        return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+      end,
+    },
+  },
 }
