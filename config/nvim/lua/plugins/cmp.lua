f293e018220f185c756018f4d5e8467ceb9eaa00
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-06-09 00:46:39 -0300

    chore(nvim): update catppuccin integrations

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -25,7 +25,7 @@ return {
     snippets = { preset = "default" },
 
     appearance = {
-      use_nvim_cmp_as_default = false,
+      use_nvim_cmp_as_default = true,
       nerd_font_variant = "normal",
       kind_icons = icons.kinds,
     },
