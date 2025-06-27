cdebbda3e25c86513513e1b57f4c39bc0661e3b5
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-06-27 12:58:02 -0300

    fix(nvim): error on load ts_context_commentstring

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -18,7 +18,9 @@ return {
       mode = "v",
     },
   },
-  opts = {
-    pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-  },
+  opts = function()
+    local ts_context_commentstring = require "ts_context_commentstring.integrations.comment_nvim"
+
+    return { pre_hook = ts_context_commentstring.create_pre_hook() }
+  end,
 }
