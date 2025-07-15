3f3b41de5ccb128fda88848e5f986e369241ae33
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-07-15 20:43:39 -0300

    refactor(btop): use action to copy btop settings

--- actions/btop.sh
+++ actions/btop.sh
@@ -8,6 +8,6 @@ CONFIG_DIR="$HOME/.config/btop"
 echo "Setting up btop..."
 
 clean_config_dir "$CONFIG_DIR"
-ln -sf "$PWD/config/btop" "$CONFIG_DIR"
+cp -r "$PWD/config/btop" "$CONFIG_DIR"
 
 echo "Btop setup complete!"
