15e140f072cf9613ea7e808eca82ddf95a6e095c
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2026-02-18 19:59:06 -0300

    fix(nvim): add workarround to import default copilot configs from lspconfig

--- config/nvim/lua/config/lsp/configs/copilot.lua
+++ config/nvim/lua/config/lsp/configs/copilot.lua
@@ -14,5 +14,20 @@ return {
     if not ai.is_copilot_provider() then
       return
     end
+
+    local file_utils = require "utils.file"
+    local lazy_intall_dir = vim.fn.stdpath "data" .. "/lazy"
+    local copilot_config_path = lazy_intall_dir .. "/nvim-lspconfig/lsp/copilot.lua"
+
+    if not file_utils.file_exists(copilot_config_path) then
+      return
+    end
+
+    local copilot_ok, copilot = pcall(dofile, copilot_config_path)
+    if not copilot_ok then
+      return
+    end
+
+    copilot.on_attach(client, bufnr)
   end,
 }
